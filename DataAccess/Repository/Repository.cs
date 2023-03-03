8bcdc6c4341ea8787a74a9a3e2c7f14e001b9118
Kron285 <91598400+Kron285@users.noreply.github.com>
2023-03-03 20:28:41 +0200

    UNIVERSITY-17 added CoursesView

---
+++ DataAccess/Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityStudyPlatform.DataAccess.Data;
+
+namespace UniversityStudyPlatform.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private ApplicationDbContext db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext _db)
+        {
+            db = _db;
+            dbSet = db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T? GetFirstOrDefault(Expression<Func<T, bool>>? filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+
+    }
+}
---
+++ UniversityStudyPlatform/Controllers/Curriculum.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityStudyPlatform.Controllers
+{
+    public class Curriculum : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
