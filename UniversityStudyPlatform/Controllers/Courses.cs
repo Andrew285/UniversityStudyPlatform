8377c33595b2c44b4c4065a16e2d0d0769a28ff6
Kron285 <91598400+Kron285@users.noreply.github.com>
2023-03-03 20:28:41 +0200

    UNIVERSITY-17 changes in HomeView

---
+++ UniversityStudyPlatform/Controllers/Courses.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityStudyPlatform.Controllers
+{
+    public class Courses : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
